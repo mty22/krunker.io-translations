395fe36e53860500dd187c80d4ee27c6e21fec76
Matthew Taylor <matt@mty.net.au>
2021-03-29 19:28:39 +1100

    Rebuild zh.js

--- zh.js
+++ zh.js
@@ -11,7 +11,6 @@ module.exports = `
     menu.btn.join 加入游戏 
     menu.btn.host 创建游戏 
     menu.btn.browser 浏览服务器 
-
     generic.wait 请稍等... 
     generic.loading 加载中... 
     generic.none 无 
@@ -30,9 +29,8 @@ module.exports = `
     generic.success 成功! 
     generic.name 名字 
     generic.level 等级 
-    generic.score 得分 
+generic.score Score
     generic.off 关 
-    generic.score 得分 
     generic.defeat 失败 
     generic.victory 胜利 
     generic.matchover 比赛结束 
@@ -60,47 +58,49 @@ module.exports = `
     generic.register 注册 
     generic.login 登录 
     generic.accuracy 命中率 
-
     player.waiting 等待更多玩家加入 
     player.died 死亡 
     player.killed 被此玩家击杀 
     player.killstreak {0} 正在 {1} 连杀 
     player.inactive 由于不活跃,游戏已断开连接 
     player.disconnect 连接已断开 
-
     timer.end 下一场将在 {0} 后开始 
     leaderboard.empty 空房间 
     popup.checkpoint 检查点! 
-
     app.play 点此开始游戏 
+app.spectate CLICK TO SPECTATE
+app.play.mobile TAP TO PLAY
+app.spectate.mobile TAP TO SPECTATE
     app.play.controller 按下开始 
-
+app.spectate.controller START to SPECTATE
     purchase.error 出现错误 
     purchase.error.click 点此</a> 并重试! 
     purchase.error.limit 您可能已超出每日交易限额 
-
     settings.reset 重置 
     settings.reset.confirm 您是否希望重置所有设定? 
     settings.import 导入 
     settings.export 导出 
     settings.none 没有找到设定 
     settings.backup 备份设定 
-
     settings.controls.change 按键绑定 
     settings.controls.press 按下任意按键 
-
     settings.local.header 本地化 <span id="requiresRestart"><span style="color: #eb5656">*</span> 需要重启游戏</span> 
     settings.local.region 默认区域 
     settings.local.lang 语言 
-
     settings.network.header 网路 
     settings.network.sendRate 高服务器应答率 
     settings.network.netRerouting 自动 
     settings.network.lagComp 延迟修正(Lag Compensation) 
-
+settings.popup.header Popups
+settings.popup.freeKRPop Show Free KR Popup
     settings.experimental.header 实验性功能 
     settings.experimental.aimFreezeFix ADS(瞄准)冻结修复 
-
+settings.experimental.mouseFlickFix Mouse Flick Fix
+settings.experimental.shaderRendering Instance Rendering
+settings.dev.header Developer
+settings.dev.forceUpdate Force Update Skins
+settings.dev.forceRegen Always Regenerate Player
+settings.dev.extraBan Extra Ban Info
     settings.chat.header 聊天 
     settings.chat.chat 显示聊天窗口 
     settings.chat.chat.all 总是 
@@ -113,7 +113,6 @@ module.exports = `
     settings.chat.chatTimer 消息显示时间 
     settings.chat.chatTextOutline 消息文字轮廓 
     settings.chat.chatFilter 不良语言过滤(仅限英文) 
-
     settings.quality.header 游戏性能 
     settings.quality.updRate 帧数限制 
     settings.quality.rendDst 渲染距离 
@@ -123,6 +122,7 @@ module.exports = `
     settings.quality.noTex 无贴图 
     settings.quality.partdist 粒子渲染距离 
     settings.quality.renderdist 渲染距离 
+settings.quality.reflection Reflection Quality
     settings.quality.antiAlias 反锯齿 
     settings.quality.shadows 阴影 
     settings.quality.softShad 软阴影 
@@ -134,14 +134,23 @@ module.exports = `
     settings.quality.flap 狙击枪瞄准镜保护盖 
     settings.quality.lowSpec 低配置模式 
     settings.quality.ambient 环境阴影 
+settings.quality.ambientOld Old Shading (When Possible)
     settings.quality.textureAnim 纹理动画 
     settings.quality.objectAnim 物体动画 
     settings.quality.screenShake 屏幕晃动 
     settings.quality.weaponShine 武器光泽 
+settings.quality.rendGamma Gamma
     settings.quality.lighting 光照效果 
+settings.quality.showExplo Show Explosions
     settings.quality.aspectRatio 屏幕高宽比 
-    settings.quality.post 后期处理 
-
+settings.post.header Post Processing
+settings.post.post Post Processing
+settings.post.bloom Bloom
+settings.post.bloomThreshold Bloom Threshold
+settings.post.bloomStrength Bloom Strength
+settings.post.bloomRadius Bloom Radius
+settings.post.ssao SSAO
+settings.post.ssaoRadius SSAO Radius
     settings.interface.hitInd 伤害指示器 
     settings.interface.hitColor 伤害指示颜色 
     settings.interface.dmg 显示伤害 
@@ -154,6 +163,7 @@ module.exports = `
     settings.interface.oldBrowser 使用旧版浏览器 
     settings.interface.rarityanim 禁用稀有物品动画,
     settings.interface.showPfp 显示用户图标 
+settings.interface.showTradeMsg Show Trade Messages
     settings.interface.hp 动态血条(HP) 
     settings.interface.dmgScale 伤害显示比例 
     settings.interface.killf 显示击杀列表 
@@ -161,6 +171,7 @@ module.exports = `
     settings.interface.net-stats 显示网络状况 
     settings.interface.fps 显示帧率(FPS) 
     settings.interface.deaths 显示死亡数 
+settings.interface.kd Show K/D Counter
     settings.interface.kills 显示击杀数 
     settings.interface.streak 显示连杀数 
     settings.interface.score 显示分数 
@@ -188,7 +199,6 @@ module.exports = `
     settings.interface.healthColE 敌人血条(HP)颜色 
     settings.interface.xpBarCol 经验条颜色 
     settings.interface.xpBarOpac 经验条透明度 
-
     settings.crosshair.header 准星 
     settings.crosshair.type 种类 
     settings.crosshair.type.2 自定义 
@@ -213,7 +223,6 @@ module.exports = `
     settings.crosshair.dot 点 
     settings.crosshair.opacity.use 使用自定义透明度 
     settings.crosshair.opacity.custom 自定义透明度 
-
     settings.hitmarker.header 命中标记 
     settings.hitmarker.color 颜色 
     settings.hitmarker.kcolor 击杀颜色 
@@ -227,18 +236,18 @@ module.exports = `
     settings.hitmarker.hitmFad 褪去速度 
     settings.hitmarker.custom 命中标记图标 
     settings.hitmarker.custom.paste 命中标记网址 
-
     settings.onKillS.style.0 默认 
     settings.onKillS.style.1 降低分辨率 
     settings.onKillS.style.2 降低FOV 
     settings.onKillS.style.3 增加FOV 
-
+settings.serverb.header Server Browser
+settings.serverb.oldBrowser Legacy Browser
+settings.serverb.defaultRegionOnly Default Region Only
     settings.gameprivacy.header 游玩隐私 
     settings.gameprivacy.streamer 主播模式 
     settings.gameprivacy.anon 匿名模式 
-
+settings.gameprivacy.hideGameTitle Hide Game Title
     settings.gameplay.header 游戏性 
-    settings.gameplay.slidCont 非帧率 Sliding 
     settings.gameplay.sensitivityX X轴灵敏度 
     settings.gameplay.sensitivityY Y轴灵敏度 
     settings.gameplay.aimsensX ADS(瞄准)X轴灵敏度 
@@ -247,9 +256,9 @@ module.exports = `
     settings.gameplay.invert 反转Y轴 
     settings.gameplay.onkillS 击杀 
     settings.gameplay.scrollDir 鼠标滚轮方向 
-
     settings.controller.header 手柄 
     settings.controller.sensitivityX X轴灵敏度 
+settings.controller.disableCons Disable Controllers
     settings.controller.sensitivityY Y轴灵敏度 
     settings.controller.aimsensX ADS(瞄准)X轴灵敏度 
     settings.controller.aimsensY ADS(瞄准)Y轴灵敏度 
@@ -259,7 +268,6 @@ module.exports = `
     settings.controller.vibration 震动 
     settings.controller.gradSpeed 渐缓速度 
     settings.controller.invert 反转Y轴 
-
     settings.mobile.header 移动设备 
     settings.mobile.sensitivityX X轴灵敏度 
     settings.mobile.sensitivityY Y轴灵敏度 
@@ -268,15 +276,16 @@ module.exports = `
     settings.mobile.vibration 震动 
     settings.mobile.gradSpeed 渐缓速度 
     settings.mobile.invert 反转Y轴 
-
     settings.audio.header 音频 
     settings.audio.sound 主音量 
     settings.audio.ambient 环境音量 
     settings.audio.voice 语言音量 
     settings.audio.weps 武器音量 
     settings.audio.player 玩家音量 
+settings.audio.skin Cosmetics Volume
     settings.audio.ui 用户界面音量 
-
+settings.audio.asset Asset Volume
+settings.audio.dialogue Action Volume
     settings.viewmodel.header 视角 
     settings.viewmodel.fov FOV(hipfire 视野) 
     settings.viewmodel.fps 武器渲染FOV 
@@ -284,8 +293,10 @@ module.exports = `
     settings.viewmodel.leaning 武器倾斜 
     settings.viewmodel.show.primary 显示主武器 
     settings.viewmodel.show.secondary 显示副武器 
+settings.viewmodel.roundArms Rounded Arms
     settings.viewmodel.show.melee 显示近战武器 
     settings.viewmodel.show.ads ADS(瞄准)时隐藏武器 
+settings.viewmodel.toggleAim Toggle Aim
     settings.viewmodel.show.hands 显示手部 
     settings.viewmodel.leftHand 左手模式 
     settings.viewmodel.aimAnim 武器ADS(瞄准)动画 
@@ -293,9 +304,10 @@ module.exports = `
     settings.viewmodel.xOff 武器X轴坐标 
     settings.viewmodel.yOff 武器Y轴坐标 
     settings.viewmodel.zOff 武器Z轴坐标 
+settings.viewmodel.weapSwapY Weapon Swap Y
+settings.viewmodel.weapReloadY Weapon Reload Y
     settings.viewmodel.yOffADS 武器ADS(瞄准)Y轴坐标 
     settings.viewmodel.adsFovMlt ADS(瞄准)FOV倍率 
-
     settings.editing.header 编辑 
     settings.editing.health.high HUD 血条颜色(高) 
     settings.editing.health.low HUD 血条颜色(低) 
@@ -307,6 +319,13 @@ module.exports = `
     settings.editing.score.shadow 弹出得分阴影 
     settings.editing.scoreOffX 弹出得分X轴坐标 
     settings.editing.scoreOffY 弹出得分Y轴坐标 
+settings.editing.prog.color Progress Bar Color
+settings.editing.prog.opacity Progress Bar Opacity
+settings.editing.prog.scale Progress Bar Scale
+settings.editing.prog.shadow Progress Bar Shadow
+settings.editing.progOffX Progress Bar X Offset
+settings.editing.progOffY Progress Bar Y Offset
+settings.editing.progRot Progress Bar Rotation
     settings.editing.saturation 饱和度 (游戏中) 
     settings.editing.menuSaturation 饱和度 (用户界面) 
     settings.editing.hue 色调 (游戏中) 
@@ -317,16 +336,20 @@ module.exports = `
     settings.editing.green 绿幕模式 
     settings.editing.tracercol 弹道轨迹 
     settings.editing.tracer 弹道轨迹 
-
+settings.editing.tracer.offset Tracer Offset
     settings.shaders.header 着色器输入 
-
     settings.mods.header 自定义 
     settings.mods.load 加载MOD 
     settings.mods.logo 允许变更Logo 
     settings.mods.auto 自动加载MOD 
     settings.mods.borders 瞄准镜边界 
+settings.mods.borderCol Scope Borders
+settings.mods.borderOpac Scope Borders Opacity
     settings.mods.scope 瞄准镜图标 
     settings.mods.scope.paste 瞄准镜图标网址 
+settings.mods.scope.opacity Scope Opacity
+settings.mods.scope.width Scope Image Width
+settings.mods.scope.height Scope Image Height
     settings.mods.dot 准星图标 
     settings.mods.dot.width 准星图标宽度 
     settings.mods.dot.height 准星图标高度 
@@ -349,7 +372,7 @@ module.exports = `
     settings.mods.damage.paste 受伤屏幕效果网址 
     settings.mods.overlay 游戏结束图标 
     settings.mods.overlay.paste 游戏叠层图片网址 
-
+settings.social.region Region Stats
     windows.settings.header 游戏设定 
     windows.mail.header Krunker邮箱 
     windows.servers.header 服务器 
@@ -359,27 +382,28 @@ module.exports = `
     windows.servers.priority 优先区域 
     windows.servers.online {0} 人游戏中 
     windows.servers.social {0} 社交中 
-
     windows.loadout.header 更换角色 
     windows.loadout.class 角色 
     windows.loadout.primary 主武器 
     windows.loadout.primary.attach 配件 
     windows.loadout.primary.reticle 准星 
+windows.loadout.primary.scope Scope
     windows.loadout.secondary 副武器 
     windows.loadout.secondary.skin 副武器皮肤 
     windows.loadout.melee 近战武器 
     windows.loadout.hat 帽子 
     windows.loadout.body 身体 
     windows.loadout.waist 腰部 
+windows.loadout.shoe Shoes
     windows.loadout.face 面部 
     windows.loadout.spray 涂鸦 
     windows.loadout.color.skin 皮肤颜色 
     windows.loadout.color.chat 聊天名字颜色 
     windows.loadout.color.hair 头发颜色 
     windows.loadout.dye 服装 
+windows.loadout.pet Pet
     windows.loadout.stat 击杀统计卡 
     windows.loadout.flag 国家旗帜 
-
     windows.mods.drop 将MOD文件放在此处或单击 
     windows.mods.paste 粘贴MOD网址 
     windows.mods.load 加载MOD 
@@ -390,10 +414,8 @@ module.exports = `
     windows.mods.load.header 加载MOD 
     windows.mods.publish 发布MOD 
     windows.mods.viewer 观看者 
-
     windows.account.header 帐户 
     windows.account.recover 联系 <a>krunker@yendis.ch</a> 找回账号密码 
-
     windows.class.header 选择角色 
     windows.controls.header 更改控制 
     windows.controls.forward 前 
@@ -417,6 +439,7 @@ module.exports = `
     windows.controls.board 排行榜 
     windows.controls.interact 交互 
     windows.controls.interactSec 次要交互 
+windows.controls.confirm Confirm Interact
     windows.controls.drop 放下(武器/物品) 
     windows.controls.wepVis 切换武器可见度 
     windows.controls.streak0 连杀 1 
@@ -428,12 +451,12 @@ module.exports = `
     windows.controls.premium1 无敌 
     windows.controls.premium2 击杀 
     windows.controls.premium3 重击 
-
+windows.controls.reset Reset Position (Race & Parkour)
+windows.controls.pretend Hide Game
     windows.host.header 创建自定义游戏 
     windows.host.maps 服务器设定 
     windows.host.cmaps 社区游戏 
     windows.host.raw 原始游戏数据 
-
     windows.host.modes 选择模式 
     windows.host.classes 选择角色 
     windows.host.settings 服务器设定 
@@ -442,28 +465,23 @@ module.exports = `
     windows.host.preset.name 预设名称 
     windows.host.start 开始游戏 
     windows.host.rehost Start Game (Bring Players) 
-
     windows.spray.header 选择涂鸦 
     windows.spray.default 默认涂鸦 
     windows.spray.spin 涂鸦 
-
     windows.maps.header 自定义游戏 
     windows.maps.none 没有找到游戏! 
     windows.maps.name 游戏名称 
     windows.maps.editor 游戏编辑器 
     windows.maps.publish 发布游戏 
     windows.maps.community 开发者社区 
-
     windows.publish.map.header 发布或更新游戏 
     windows.publish.map.thumbnail 没有缩略图 
     windows.publish.map.paste 粘贴游戏数据 
     windows.publish.map.update 发布/更新 
     windows.publish.map.login 创建并上传游戏! 
-
     windows.theatre.header 影院模式 
     windows.theatre.record 启用录制 
     windows.theatre.key 录制按键 
-
     windows.clans.header 工会 
     windows.clans.members 成员 ({0}) 
     windows.clans.requests 申请 
@@ -474,7 +492,6 @@ module.exports = `
     windows.clans.view 浏览工会 
     windows.clans.join 加入工会 
     windows.clans.login 创建并加入工会! 
-
     windows.store.header 商店 
     windows.store.spin {0} 旋转/开箱 
     windows.store.agree 通过此次购买，您将同意 {0} 条款. 
@@ -493,7 +510,9 @@ module.exports = `
     windows.store.login 用于在商店中购买和解锁物品! 
     windows.store.purchasing 购买中... 
     windows.store.unboxed {0}开箱解锁了{1} 
-
+windows.trade.header Trading
+windows.scope.header Select Scope
+windows.premium.header Premium
     windows.skin.header 选择皮肤 
     windows.hat.header 选择帽子 
     windows.back.header 选择背部饰品 
@@ -504,84 +523,66 @@ module.exports = `
     windows.reticle.header 选择准星 
     windows.reticle.unlocked 于{0}级结束 
     windows.reticle.req 需要{0}级 
-
     windows.dye.header 选择服装 
     windows.dye.default 默认服装 
     windows.dye.spin 服装 
-
     windows.changelog.header 变更日志 
-
     windows.mods.community.header 社区MOD 
     windows.mods.community.fav 最爱 
     windows.mods.community.new 最新MOD 
     windows.mods.community.hot 最热门 
     windows.mods.community.name MOD名称 
     windows.mods.community.none 没有找到MOD 
-
     windows.publish.mod.header 发表MOD 
     windows.publish.mod.thumbnail 没有缩略图 
     windows.publish.mod.name MOD名称 
     windows.publish.mod.paste MOD网址 
     windows.publish.mod.update 发表/更新 
     windows.publish.mod.login 以创建并上传! 
-
     windows.secondary.header 选择次要武器 
     windows.secondary.unlocked 已解锁 
     windows.secondary.req 需要至少{0}级 
-
     windows.advertise.header 广告 
     windows.support.header 支持 
     windows.players.header 玩家列表 
     windows.players.none 当前没有玩家... 
-
     windows.join.header 加入 
     windows.join.code 输入游戏 网址/代码 
-
     windows.client.header 客户端 
     windows.client.windows Windows游戏安装器 
     windows.client.mac macOS游戏安装器 
     windows.client.linux Linux游戏安装器 
-
     windows.ranked.header 排位 
     windows.ranked.login 以进行排位赛! 
     windows.ranked.level 您至少需要 <span color="black"> {0} 级</span> 以进行排位赛! 当前您的等级是 <span color="black"> {1} 级</span>. 
     windows.ranked.mode.r1v1 单排 
     windows.ranked.mode.r2v2 双排 
     windows.ranked.mode.r4v4 四排 
-
     windows.chall.header 挑战 
     windows.chall.login 以完成挑战! 
-
     thumbnail.upload.limit 失败. {0}kb 缩略图限制 
     image.upload.limit 失败. {0}kb 图片限制 
     thumbnail.upload.error 缩略图错误 
-
     mod.extracting 提取中 {0}/{1} 
     mod.disabled MOD已禁用 
     mod.error MOD错误! 
     mod.loading 加载MOD中... 
     mod.invalid 无效MOD文件! 
-
     matchmaker.full 满 
     matchmaker.full2 游戏已满. 
     matchmaker.updating 游戏更新中 
     matchmaker.invalid 没有找到游戏. 
     matchmaker.none 服务器已达上限 
-
     adblocker 禁用广告拦截器以获得免费的KR. 
     ad-failed 广告加载失败. 请稍后重试. 
-
     streamers.views {0} 观看者 
     streamers.none 当前没有Twitch直播! 
-
     error.extentions 如果您有任何(浏览器)插件, 请禁用它们 
     error.seek 点此以搜索新的游戏 
-
     custom.map.upload.limit 失败. 角色游戏数据限制为 500k 
     custom.map.missing 请选择至少一种游戏或地图 
     custom.mode.missing 请选择至少一种模式 
     custom.class.missing 请选择至少一种角色 
-
     server.config.maxPlayers 最大玩家数 
     server.config.minPlayers 最小玩家数 
     server.config.lives 生命数 
@@ -594,15 +595,23 @@ module.exports = `
     server.config.airStrf Air Strafing 
     server.config.deltaMlt 时间倍率 
     server.config.strafeSpd Strafe 速度 
-
     server.config.fiRat 武器射速 
     server.config.reSpd 换弹速度 
     server.config.slidTime Slide 时间 
-
+server.config.fixMov Fixed Movement
+server.config.slidSpd Slide Speed
     server.config.healthMlt 血量倍率 
     server.config.impulseMlt 武器后坐力 
     server.config.nameTeam1 队伍 1 名字 
     server.config.nameTeam2 队伍 2 名字 
+server.config.nameTeam3 Team 3 Name
+server.config.nameTeam4 Team 4 Name
+server.config.nameTeam5 Team 5 Name
+server.config.t1Dmg Team 1 Damage
+server.config.t2Dmg Team 2 Damage
+server.config.t3Dmg Team 3 Damage
+server.config.t4Dmg Team 4 Damage
+server.config.t5Dmg Team 5 Damage
     server.config.selTeam 选择队伍 
     server.config.allowSpect 观战 
     server.config.killRewards 击杀奖励 
@@ -618,14 +627,19 @@ module.exports = `
     server.config.nameTags 隐藏名牌 
     server.config.noReload 无需重装弹药 
     server.config.private 私人 
+server.config.krRewards KR Rewards
     server.config.warmupTime 热身时间 
+server.config.objtvTime Objective Time
     server.config.forceSpawn 自动重生时间 
     server.config.scoreLimit 得分限制 
+server.config.keepTScore Keep Team Score
+server.config.forceC Force Mode Classes
+server.config.logTim Log Time (Race, Parkour)
     server.config.hitBoxPad 击中判断模型倍率(hitbox) 
     server.config.hpRegen 血量回复 
     server.config.disableB 解除地图边界 
     server.config.throwMel 飞刀 
-
+server.config.noCosm Disable Cosmetics
     social.login 登录/注册 
     social.login.failed 登录失败 
     social.login.buy 登录后购买 
@@ -635,10 +649,8 @@ module.exports = `
     social.since 自从 
     social.user.maps 用户游戏 
     social.user.mods 用户MOD 
-
     social.maps.none 没有找到游戏... 
     social.mods.none 没有找到MOD... 
-
     social.market.price.low 最低价格 
     social.market.price.list 输入价格 
     social.market.searching 搜索 
@@ -656,6 +668,8 @@ module.exports = `
     social.market.back 背部饰品 
     social.market.waist 腰部饰品 
     social.market.face 面部饰品 
+social.market.shoe Shoe Item
+social.market.pet Pet Item
     social.market.melee 近战武器 
     social.market.spray 涂鸦物品 
     social.market.dye 服装物品 
@@ -666,21 +680,7 @@ module.exports = `
     social.market.filter 搜索 
     social.market.sort2 排序 
     social.market.price.high 最高价格 
-
     social.trading.submitting 提交报价中... 
-
-    queue.status.queuing 排队中 
-    queue.status.ready-up 队伍没有准备就绪 
-    queue.status.no-queues 请选择排位模式 
-    queue.status.waiting-for-leader 等待队长决定 
-    queue.status.queued 寻找比赛 [{0} 人在队列中] 
-    // 'queue.status.queued-subtext 需要的 {1} 人中 #{1} 人在队列中 
-    // 'queue.status.queued-subtext {0}\n{1} 在队列中 
-    queue.status.creating-game 创建游戏
-    queue.status.host 创建游戏 
-    queue.status.join 加入游戏 
-    queue.status.already-queued 您已在另一个浏览器标签中排队 
-
     social.market.gift.amount 赠送金额 
     social.market.fee.gift 无费用 
     social.market.gift 赠送KR 
