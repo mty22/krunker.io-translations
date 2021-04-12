82edf20a2b258c21eec1ff06d609b6953bbb63a6
Matthew Taylor <matt@mty.net.au>
2021-04-12 16:15:22 +1000

    Rebuild fr.js (French) based on en.js (v3.8.1)

--- fr.js
+++ fr.js
@@ -326,6 +326,13 @@ settings.editing.prog.shadow Progress Bar Shadow
 settings.editing.progOffX Progress Bar X Offset
 settings.editing.progOffY Progress Bar Y Offset
 settings.editing.progRot Progress Bar Rotation
+settings.editing.progm.color Charge Bar Color
+settings.editing.progm.opacity Charge Bar Opacity
+settings.editing.progm.scale Charge Bar Scale
+settings.editing.progm.shadow Charge Bar Shadow
+settings.editing.progmOffX Charge Bar X Offset
+settings.editing.progmOffY Charge Bar Y Offset
+settings.editing.progmRot Charge Bar Rotation
 settings.editing.saturation Saturation (en jeu)
 settings.editing.menuSaturation Saturation (interface)
 settings.editing.hue Teinte (en jeu)
@@ -392,7 +399,7 @@ windows.loadout.secondary Arme secondaire
 windows.loadout.secondary.skin Skin de l’arme secondaire
 windows.loadout.melee Skin de l’arme de mêlée
 windows.loadout.hat Chapeau
-windows.loadout.body Corps
+windows.loadout.back Corps
 windows.loadout.waist Bassin
 windows.loadout.shoe Shoes
 windows.loadout.face Tête
@@ -625,9 +632,11 @@ server.config.bDrop Gravité des projectiles
 server.config.thirdPerson Troisième personne
 server.config.requireCaptcha Captcha requis
 server.config.nameTags Cacher les pseudos
+server.config.kCams Kill Cams
 server.config.noReload Munitions illimitées
 server.config.private Privé
 server.config.krRewards KR Rewards
+server.config.forceDefault Force Default Settings
 server.config.warmupTime Temps d’échauffement
 server.config.objtvTime Objective Time
 server.config.forceSpawn Temps de réapparition automatique
@@ -685,4 +694,54 @@ social.market.gift.amount Montant du don
 social.market.fee.gift Aucun frais
 social.market.gift Offrir des KR
 social.market.gift.msg Laisser un message
+respawn.auto Auto Respawning in {0}
+custom.host.ranked Can't host match while in ranked.
+funds.insufficient Insufficient Funds
+funds.out It appears you have run out of Krunkies.
+custom.passcode.long Password too long
+custom.passcode.input Enter Server Password to join
+custom.tryagain Please try again.
+purchase.kr.success Added {0} KR to your account
+limited.purchase.ask Are you sure you want to purchase [{0}] for {1} KR?
+limited.purchase.success Added {0} to your inventory
+pretend.exit press [{0}] again to exit
+settings.loaded Settings Loaded
+settings.loaded.none No settings found
+account.login.required Must be logged in
+account.banned.hack This account has been banned for Hacking.
+account.banned.appeal If you want to appeal your ban, contact:
+account.delete You may request to delete your account, if the request is not cancelled within 14 days the account and all associated data will be permanently removed.
+account.delete.request Are you sure you want to request an account deletion?
+account.delete.cancel Are you sure you want to cancel account deletion?
+spin.free.available FREE KR AVAILABLE!
+spin.free.claim This spin is available to claim once per Hour after completing a match.
+spin.free.disable These Popups can be disabled in General Settings
+mod.reset.0 Will take effect after respawning.
+mod.reset.1 Map Objects dont get reset
+windows.profile.picture Change Picture
+stats.assists Assists
+stats.nukes Nukes
+stats.melee Melee
+stats.fistkills Beatdowns
+stats.thrownkills Bullseyes
+stats.headshots Headshots
+stats.wallbangs Wallbangs
+stats.sprays Sprays Placed
+windows.host.default.settings Enabling Default Settings allows for storing Player Progress and Highscores
+windows.host.default.commands *Host Commands are Disabled in this mode: /noclip /god /smite etc
+windows.host.rewards Enabling KR Rewards forces the Server to use Default Settings & allows players to earn KR from the map funds:
+windows.store.wheels.sub Try your luck and spin for a chance to win some new skins!
+windows.store.currency * Prices in USD can be converted to your local currency at an exchange rate defined by the payment provider or bank
+windows.store.creator Consider Support your favorite Content Creator by using a Creator Code
+windows.chall.customs Challenges can't be completed in Custom Matches
+twitch.link Are you sure you want to link a twitch account?
+twitch.unlink Are you sure you want to unlink your twitch account?
+windows.clan.leave Are you sure you want to leave the clan?
+windows.clan.delete Are you sure you want to delete the clan?
+windows.clan.kick Are you sure you want kick {0} from the clan?
+windows.store.premium.purchase Are you sure you want to purchase {0} days of Premium?
+windows.playerlist.ban Are you sure you want ban {0} from this game?
+windows.playerlist.flag Are you sure you want to Flag {0}?
+windows.playerlist.unflag Are you sure you want to Unflag {0}?
+windows.playerlist.punish Are you sure you want to take action on {0}?
 `;
