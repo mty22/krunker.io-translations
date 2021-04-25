1bf7238be689c9550ebdb2cd39065466371549ce
Matt Taylor <matt@idle.host>
2021-04-25 20:51:52 +1000

    Move Bulgarian to WIP

--- non-approved-languages/README.md
+++
@@ -1,1 +0,0 @@
-Any language added to this folder should be regarded as unchecked and or new to the game.
--- non-approved-languages/non-approved-languages.js
+++
@@ -1,4 +0,0 @@
-// LANGUAGES -- note: this is just a template for keeping track of filename to language name:
-module.exports.langNames = {
-    "bg": "Bulgarian",
-}
