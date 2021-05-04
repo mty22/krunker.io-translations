e3103cba501c285cf22337bc84341bb889e76704
Matt Taylor <matt@idle.host>
2021-05-04 22:18:31 +1000

    Add simple link generator tool.

---
+++ tools/release_links_generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Quick and easy links to languages based on release tag provided.
+# by github.com/mty22
+
+release_ver="$1"
+
+if [ -z "$release_ver" ]; then
+	echo "Script usage: $0 <version>."
+	echo "Example: $0 3.8.5-live"
+	exit 1
+fi
+
+# Assuming you are working from the github root directory..
+for y in $(grep '\:' languages.js | sed -e 's/ //g'|egrep -vi 'LANG|en.js' ); do 
+	lang_short=$(echo $y | cut -d\" -f2) 
+	lang_long=$(echo $y | cut -d\" -f4)
+	echo "${lang_long}: https://raw.githubusercontent.com/mty22/krunker.io-translations/${release_ver}/${lang_short}.js"
+done
