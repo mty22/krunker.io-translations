26b00e3dd039a57ffcc3e34a2cf9b9a24f814e00
nickauthentic <82802046+nickauthentic@users.noreply.github.com>
2022-02-02 21:10:08 +0100

    Update nl.js
    Added some new translations :)

--- nl.js
+++ nl.js
@@ -91,15 +91,15 @@ settings.network.header Netwerk
 settings.network.netRerouting Auto Routing (Beïnvloed Ping)
 settings.network.netRate Netwerk snelheid (Hz)
 settings.network.lagComp Lag Compensatie
-settings.network.optNet Optimized Networking
+settings.network.optNet Geoptimaliseerde Netwerken
 settings.popup.header Popups
 settings.experimental.header Experimenteel
-settings.experimental.holiday Holiday Effects
+settings.experimental.holiday Holiday Effecten
 settings.experimental.aimFreezeFix Aim Freeze Fix
 settings.experimental.mouseFlickFix Muis Flick Fix
 settings.experimental.shaderRendering Instance Rendering
-settings.experimental.mouseAccel Mouse Acceleration
-settings.experimental.tbInterp TB Interpolation
+settings.experimental.mouseAccel Muis Versnelling
+settings.experimental.tbInterp TB Interpolatie
 settings.client.header Client
 settings.client.unlimitedFrames Unlimited FPS
 settings.client.gameCap Game Capture
@@ -113,7 +113,7 @@ settings.client.angleBackend ANGLE Graphics Backend
 settings.client.colorProfile Color Profile
 settings.client.resourcePacks Resource Packs
 settings.dev.header Developer
-settings.dev.forceUpdate Force Update Skins
+settings.dev.forceUpdate Forceer Update Skins
 settings.dev.forceRegen Altijd Speler Regenereren
 settings.dev.extraBan Extra Ban Info
 settings.chat.header Chat
@@ -173,7 +173,7 @@ settings.interface.dmgCol Schade Kleur
 settings.interface.critCol Crit Kleur
 settings.interface.header Interface
 settings.interface.scale UI Grootte
-settings.interface.theme Game Theme
+settings.interface.theme Game Thema
 settings.interface.ui Toon UI
 settings.interface.scoreboard Gebruik Oud Scorebord
 settings.interface.oldBrowser Gebruik Oude Browser
@@ -206,7 +206,7 @@ settings.interface.hideNames.enemy Alleen Tegenstanders
 settings.interface.hideNames.all Iedereen
 settings.interface.nametagStyle Naam Stijl
 settings.interface.nametagOpac Naam Ondoorzichtigheid
-settings.interface.nametagScale Nametag Scale
+settings.interface.nametagScale Nametag Grootheid
 settings.interface.nametagStyle.health Alleen Gezondheid
 settings.interface.nametagStyle.name Alleen naam
 settings.interface.nametagStyle.nameLvl Alleen naam en level
@@ -217,13 +217,13 @@ settings.interface.healthColE Naam Gezondheid Kleur Tegenstanders
 settings.interface.xpBarCol XP Bar Kleur
 settings.interface.xpBarCol2 XP Bar Kleur 2
 settings.interface.xpBarOpac XP Bar Doorzichtigheid
-settings.medals.header Medals
-settings.medals.show Show Medals
-settings.medals.classic Classic Medals
-settings.medals.sound Play Medal Sounds
-settings.medals.scale Medal Scale
-settings.medals.xOff Medal X Offset
-settings.medals.yOff Medal Y Offset
+settings.medals.header Medailles
+settings.medals.show Show Medailles
+settings.medals.classic Klassieke Medailles
+settings.medals.sound Speel Medaille Geluid
+settings.medals.scale Medaille Grootte
+settings.medals.xOff Medaille X Offset
+settings.medals.yOff Medaille Y Offset
 settings.crosshair.header Crosshair
 settings.crosshair.type Soort
 settings.crosshair.type.1 Dynamisch
@@ -300,12 +300,12 @@ settings.gameprivacy.header Gameplay Privacy
 settings.gameprivacy.streamer Streamer Modus
 settings.gameprivacy.anon Anonieme Modus
 settings.gameprivacy.hideGameTitle Verstop Game Titel
-settings.gameprivacy.showRegion Show Region Info
+settings.gameprivacy.showRegion Laat Regio Info Zien
 settings.gameprivacy.verified Verified Badge Weergeven
 settings.gameprivacy.premium Premium Badge Weergeven
 settings.gameprivacy.partner Partner Badge Weergeven
 settings.gameprivacy.rank Ranked Badge Weergeven
-settings.gameprivacy.custom Show Custom Badge
+settings.gameprivacy.custom Custom Badge Weergeven
 settings.gameplay.header Gameplay
 settings.gameplay.gpSetts Gameplay Instellingen
 settings.gameplay.sensitivityX X Gevoeligheid
@@ -342,7 +342,7 @@ settings.audio.ambient Omringende Volume
 settings.audio.voice Stem Volume
 settings.audio.input Audio/Mic Invoer
 settings.audio.mic Mic Volume
-settings.audio.micquality Mic Quality
+settings.audio.micquality Mic Kwaliteit
 settings.audio.weps Wapen Volume
 settings.audio.player Speler Volume
 settings.audio.skin Cosmetica Volume
@@ -480,7 +480,7 @@ windows.loadout.color.chat Chatnaam Kleur
 windows.loadout.color.hair Haarkleur
 windows.loadout.dye Kleurstof
 windows.loadout.pet Huisdier
-windows.loadout.wrist Wrist
+windows.loadout.wrist Pols
 windows.loadout.stat Killkaart Stat
 windows.loadout.flag Landvlag
 windows.loadout.badge Custom Badge
@@ -507,7 +507,7 @@ windows.controls.aim1 Richt
 windows.controls.shoot1 Schiet
 windows.controls.inspect Inspecteren
 windows.controls.spray Spray
-windows.controls.sprayWheel Spray Wheel (Hold)
+windows.controls.sprayWheel Spray Wiel (Vasthouden)
 windows.controls.jump Spring
 windows.controls.crouch Bukken
 windows.controls.prim Primair wapen
@@ -539,15 +539,15 @@ windows.controls.streak2 Streak 3
 windows.controls.streak3 Streak 4
 windows.controls.streak4 Streak 5
 windows.controls.taunt0 Taunt (Psst)
-windows.controls.taunt1 Taunt (Whistle)
-windows.controls.taunt2 Taunt (Fart)
+windows.controls.taunt1 Taunt (Fluitje)
+windows.controls.taunt2 Taunt (Scheet)
 windows.controls.taunt3 Taunt (Meme)
 windows.controls.taunt4 Taunt (WTF is This?)
 windows.controls.taunt5 Taunt (Right Now!)
 windows.controls.prop Selecteer Prop (Houd)
-windows.controls.propRand Select Random Prop
-windows.controls.propRot Toggle Rotation
-windows.controls.propRotR Reset Rotation
+windows.controls.propRand Selecteer Willekeurig Prop
+windows.controls.propRot Schakel Rotatie
+windows.controls.propRotR Reset Rotatie
 windows.controls.premium0 Noclip
 windows.controls.premium1 Godmode
 windows.controls.premium2 Dood
@@ -611,7 +611,7 @@ windows.store.amount {0} KR in account
 windows.store.login om items uit de winkel te kopen en te ontgrendelen!
 windows.store.purchasing Inkoop ...
 windows.store.unboxed {0} {1} geopend
-windows.store.found {0}{1} found
+windows.store.found {0}{1} gevonden
 windows.trade.header Ruilen
 windows.scope.header Selecteer Scope
 windows.premium.header Premium
@@ -670,12 +670,12 @@ mod.error mod error!
 mod.loading mod laden...
 mod.invalid ongeldig mod bestand!
 matchmaker.full VOL
-matchmaker.GameFull Game is full.
-matchmaker.NoServersMatchQuery GAME UPDATING
-matchmaker.InvalidGameId Game not found.
-matchmaker.NoAvailableServers Servers are at Max Capacity
+matchmaker.GameFull Game is vol.
+matchmaker.NoServersMatchQuery GAME AAN HET UPDATEN
+matchmaker.InvalidGameId Game niet gevonden.
+matchmaker.NoAvailableServers Servers zijn op maximale capaciteit
 matchmaker.GameIssue Unknown Join Issue
-adblocker Doe je ad blocker uit om gratis Krunkies te krijgen.
+adblocker Zet je ad blocker uit om gratis Krunkies te krijgen.
 ad-failed Ad Mislukt om te laden. Probeer Opnieuw!
 streamers.views {0} kijkers
 streamers.none Geen actieve streams!
@@ -728,13 +728,13 @@ server.config.thirdPerson 3e Persoon
 server.config.requireCaptcha Captcha Vereist
 server.config.nameTags Verstop Naamplaatjes
 server.config.kCams Kill Camera
-server.config.aAnon Allow Anonymous
+server.config.aAnon Anoniem Toestaan
 server.config.noReload Niet Herladen
 server.config.private Privé
 server.config.krRewards KR Beloningen
 server.config.forceDefault Dwing Standaard Instellingen
 server.config.warmupTime Opwarm Tijd
-server.config.gamRounds Rounds
+server.config.gamRounds Rondes
 server.config.objtvTime Objectief Tijd
 server.config.forceSpawn Auto Respawn Tijd
 server.config.intermTmr Onderbreking (s)
@@ -843,7 +843,7 @@ windows.host.rewards Door KR Beloningen in te schakelen, wordt de server gedwong
 windows.store.wheels.sub Beproef je geluk en spin om kans te maken op nieuwe skins!
 windows.store.currency *Prijzen in USD kunnen worden omgerekend naar je lokale valuta tegen een wisselkoers die is bepaald door de betalingsprovider of bank
 windows.store.creator Consider Overweeg je favoriete Content Creator te helpen door een Creator Code te gebruiken
-windows.chall.customs Challenges kunnen niet worden voltooid in Custom Matches
+windows.chall.customs Challenges kunnen niet worden voltooid in Custom Games
 twitch.link Weet je zeker dat je een Twitch account wilt linken?
 twitch.unlink Weet je zeker dat je jouw Twitch account with unlinken?
 windows.clan.leave Weet je zeker dat je de clan wilt verlaten?
@@ -888,27 +888,27 @@ social.trades.message SCHAKEL RUILBERICHTEN IN IN JE INSTELLINGEN
 social.trades.warning Je Krijgt Geen Items Van Deze Trade!
 social.trades.accept Weet Je Zeker Dat Je Deze Trade Wilt Accepteren?
 stats.airdrops KR-Packages
-stats.airdrops.stolen Airdrops Stolen
+stats.airdrops.stolen Airdrops Gestolen
 settings.quality.flapAnim Sniper Flap Animatie
 chall.name.hs Get {0} headshots {1}
 chall.name.ns Get {0} no-scopes {1}
-chall.name.ls Get {0} legshots {1}
-chall.name.tmk Get {0} throwing knife kills {1}
-chall.name.fb Get {0} first bloods {1}
+chall.name.ls Get {0} beenschoten {1}
+chall.name.tmk Get {0} mes werp kills {1}
+chall.name.fb Get {0} eerste kills {1}
 chall.name.mk Get {0} melee kills {1}
-chall.name.fk Get {0} fist kills {1}
+chall.name.fk Get {0} vuist kills {1}
 chall.name.qs Get {0} quickscopes {1}
-chall.name.wb Get {0} wallbangs {1}
-chall.name.airk Get {0} mid-air kills {1}
+chall.name.wb Get {0} muurschoten {1}
+chall.name.airk Get {0} mid-lucht kills {1}
 chall.name.lns Get {0} longshots {1}
 chall.name.tk Get {0} turbo kills {1}
 chall.name.drk Get {0} drift kills {1}
 chall.name.kstk Get a {0} killstreak {1}
-chall.name.w Win {0} matches
-chall.name.d Die {0} times
+chall.name.w Win {0} games
+chall.name.d Die {0} keer
 chall.name.score Get {0} score
-chall.name.dmg Deal {0} damage {1}
+chall.name.dmg Deal {0} schade {1}
 chall.name.bzk Get {0} buzz kills {1}
 chall.name.cc Get {0} close-calls {1}
-chall.name.rk Deal {0} revenge kills {1}
+chall.name.rk Deal {0} wraak kills {1}
 `;
