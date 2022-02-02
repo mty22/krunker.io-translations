017696de713e6cf689bda64c84143aa26a6ac9d3
nerrrrrrrdy <98909236+nerrrrrrrdy@users.noreply.github.com>
2022-02-02 19:47:47 +0100

    french translation update
    translated challenge requirements and more stuff

--- fr.js
+++ fr.js
@@ -91,15 +91,15 @@ settings.network.header Réseau
 settings.network.netRerouting Routage automatique (affecte le Ping)
 settings.network.netRate Taux de rafraîchissement réseau (Hz)
 settings.network.lagComp Compensation du lag
-settings.network.optNet Optimized Networking
+settings.network.optNet Réseautage optimizé
 settings.popup.header Popups
 settings.experimental.header Expérimental
-settings.experimental.holiday Holiday Effects
+settings.experimental.holiday Éffets de fêtes
 settings.experimental.aimFreezeFix Correction du bug de visée bloquée
 settings.experimental.mouseFlickFix Correction des accoups de la souris
 settings.experimental.shaderRendering Instanciation géométrique
-settings.experimental.mouseAccel Mouse Acceleration
-settings.experimental.tbInterp TB Interpolation
+settings.experimental.mouseAccel Accélération de la souris
+settings.experimental.tbInterp  Interpolation TB
 settings.client.header Client
 settings.client.unlimitedFrames FPS illimités
 settings.client.gameCap Capture du jeu
@@ -111,7 +111,7 @@ settings.client.resetSettings Réinitialiser les paramètres du client
 settings.client.acceleratedCanvas Canvas accélérés
 settings.client.angleBackend ANGLE Graphics Backend
 settings.client.colorProfile Profil de couleur
-settings.client.resourcePacks Resource Packs
+settings.client.resourcePacks Packs de resources
 settings.dev.header Développeur
 settings.dev.forceUpdate Forcer la mise à jour des skins
 settings.dev.forceRegen Toujours régénérer le joueur
@@ -173,7 +173,7 @@ settings.interface.dmgCol Couleur des dégâts
 settings.interface.critCol Couleur des critiques
 settings.interface.header Interface
 settings.interface.scale Taille de l'interface
-settings.interface.theme Game Theme
+settings.interface.theme Theme du jeu
 settings.interface.ui Afficher l'interface
 settings.interface.scoreboard Ancien tableau des scores
 settings.interface.oldBrowser Ancien navigateur de serveurs
@@ -217,13 +217,13 @@ settings.interface.healthColE Couleur de vie des ennemis
 settings.interface.xpBarCol Couleur de la barre d'xp
 settings.interface.xpBarCol2 Couleur 2 de la barre d'xp
 settings.interface.xpBarOpac Opacité de la barre d'xp
-settings.medals.header Medals
-settings.medals.show Show Medals
-settings.medals.classic Classic Medals
-settings.medals.sound Play Medal Sounds
-settings.medals.scale Medal Scale
-settings.medals.xOff Medal X Offset
-settings.medals.yOff Medal Y Offset
+settings.medals.header Medals Médailles
+settings.medals.show Show Medals Montrer les médeailles
+settings.medals.classic Médailles classiques
+settings.medals.sound Jouer les sons des médailles
+settings.medals.scale Taille des médailles
+settings.medals.xOff Décalage des médailles X
+settings.medals.yOff Décalage des médailles Y
 settings.crosshair.header Curseur
 settings.crosshair.type Type
 settings.crosshair.type.1 Dynamique
@@ -342,7 +342,7 @@ settings.audio.ambient Volume ambiant
 settings.audio.voice Volume des voix
 settings.audio.input Entrée audio/micro
 settings.audio.mic Volume micro
-settings.audio.micquality Mic Quality
+settings.audio.micquality Qualité du micro
 settings.audio.weps Volume des armes
 settings.audio.player Volume des joueurs
 settings.audio.skin Volume des cosmétiques
@@ -816,7 +816,7 @@ pretend.exit Appuyer sur [{0}] de nouveau pour quitter
 settings.loaded Paramètres chargés
 settings.loaded.none Pas de paramètres trouvés
 account.login.required Doit être connecté
-account.banned.hack Ce compte a été banni pour cheat.
+account.banned.hack Ce compte a été banni pour tricherie.
 account.banned.appeal Si vous souhaitez faire une demande de débannissement, contactez:
 account.delete Vous pouvez demander la suppression de votre compte, si la requête n'est pas annulée sous 14 jours, le compte et les données associées à celui-ci seront supprimées.
 account.delete.request Êtes-vous sûr de vouloir envoyer la requête de supression du compte ?
@@ -831,12 +831,12 @@ stats.assists Aides
 stats.nukes Nukes
 stats.melee Mêlées
 stats.fistkills Kills aux points
-stats.thrownkills Kills aux jets de mêlée
+stats.thrownkills Kills aux lancers de mêlée
 stats.headshots Tirs à la tête
 stats.wallbangs Tirs à travers les murs
-stats.crouches Crouches
+stats.crouches Accroupissements
 stats.sprays Tags placés
-stats.kpd Helpful Reports
+stats.kpd Helpful Reports Plaintes utiles
 windows.host.default.settings Activer les paramètres par défaut permet le stockage du progrès des joueurs et des meilleurs scores.
 windows.host.default.commands *Les commandes d'host sont désactivées dans ce mode: /noclip /god /smite etc
 windows.host.rewards Activer les récompenses KR force le serveur à utiliser les paramètres par défaut et permet aux joueurs de gagner des KR depuis les fonds de la map:
@@ -887,28 +887,28 @@ social.trades.decline Refuser tout
 social.trades.message ACTIVER LES MESSAGES D'ÉCHANGES DANS LES PARAMÈTRES
 social.trades.warning Vous ne recevez rien en échange!
 social.trades.accept Êtes-vous sûr de vouloir accepter cette offre?
-stats.airdrops Paquets de KR
-stats.airdrops.stolen Airdrops Stolen
+stats.airdrops Largage aérien de KR
+stats.airdrops.stolen Airdrops Stolen Largages aériens de KR volés
 settings.quality.flapAnim Animation du rabat de sniper
-chall.name.hs Get {0} headshots {1}
-chall.name.ns Get {0} no-scopes {1}
-chall.name.ls Get {0} legshots {1}
-chall.name.tmk Get {0} throwing knife kills {1}
-chall.name.fb Get {0} first bloods {1}
-chall.name.mk Get {0} melee kills {1}
-chall.name.fk Get {0} fist kills {1}
-chall.name.qs Get {0} quickscopes {1}
-chall.name.wb Get {0} wallbangs {1}
-chall.name.airk Get {0} mid-air kills {1}
-chall.name.lns Get {0} longshots {1}
-chall.name.tk Get {0} turbo kills {1}
-chall.name.drk Get {0} drift kills {1}
-chall.name.kstk Get a {0} killstreak {1}
-chall.name.w Win {0} matches
-chall.name.d Die {0} times
-chall.name.score Get {0} score
-chall.name.dmg Deal {0} damage {1}
+chall.name.hs  Obtenez {0} tirs a la tête {1}
+chall.name.ns  Obtenez {0} tirs sans viser {1}
+chall.name.ls  Obtenez {0} tirs a la jambe {1}
+chall.name.tmk Obtenez {0} éliminations au lancer de couteau {1}
+chall.name.fb  Obtenez {0} premier sangs {1}
+chall.name.mk  Obtenez {0} éliminations à la mêlée
+chall.name.fk  Obtenez {0} éliminations au poing {1}
+chall.name.qs  Obtenez {0} éliminations à la visée rapide {1}
+chall.name.wb  Obtenez {0} éliminations à travers un mur {1}
+chall.name.airk Obtenez {0} éliminations en l'air {1}
+chall.name.lns Obtenez {0} tirs de loin
+chall.name.tk  Obtenez {0} éliminations turbo {1}
+chall.name.drk Obtenez {0} éliminations derapantes {1}
+chall.name.kstk Obtenez une serie d'éliminations de {0} {1}
+chall.name.w Gagnez {0} matchs
+chall.name.d Mourrez {0} fois
+chall.name.score Obtenez {0} score
+chall.name.dmg Faire {0} dégâts {1}
 chall.name.bzk Get {0} buzz kills {1}
-chall.name.cc Get {0} close-calls {1}
-chall.name.rk Deal {0} revenge kills {1}
+chall.name.cc Obtenez {0} échappés-belles {1}
+chall.name.rk Faire {0} éliminations de revenge {1}
 `;
