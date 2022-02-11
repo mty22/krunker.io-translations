9e99dd9de97bab51f23930367eb489c1510cbe6b
DetectGamer <85982335+DetectGamer@users.noreply.github.com>
2022-02-11 17:15:18 +0200

    fi.js updated
    Sorry for so many commits, i am still learning this application

---
+++ fi.js
@@ -0,0 +1,281 @@
+ module​.​exports​ ​=​ ​`
+ generic.connecting YHDISTETÄÄN... 
+ ​player.inactive Heitetty ulos toimettomuuden vuoksi 
+ ​timer.end Seuraava peli alkaa {0}:n kuluttua 
+ ​leaderboard.empty Tyhjä Serveri 
+ ​app.play KLIKKAA PELATAKSESI 
+ ​app.spectate KLIKKAA KATSELLAKSESI 
+ ​app.spectate.mobile NAPAUTA KATSELLAKSESI 
+ ​app.play.controller PAINA START ALOITTAAKSESI 
+ ​app.spectate.controller PAINA START KATSELLAKSESI 
+ ​settings.network.netRate Netin Taajuus (Hz) 
+ ​settings.experimental.header Kokeelliset Asetukset 
+ ​settings.experimental.holiday Loma-Efektit 
+ ​settings.network.optNet Optimoitu Nettiyhteys 
+ ​settings.experimental.mouseFlickFix Hiiren Nopean Liikahtamisen Korjaus 
+ ​settings.experimental.mouseAccel Hiiren Kiihtyvyys 
+ ​settings.experimental.tbInterp TB Interpolaatio 
+ ​settings.experimental.mouseFlickFix Hiiren Nopean Liikahtamisen Korjaus 
+ ​settings.client.gameCap Tallenna Ottelu 
+ ​settings.client.resetSettings Resetoi Ohjelman Asetukset 
+ ​settings.client.acceleratedCanvas Kiihdytetty Grafiikanlataus 
+ ​settings.client.resourceSwapper Resurssien Vaihtaja 
+ ​settings.client.resetSettings Resetoi Ohjelman Asetukset 
+ ​settings.client.acceleratedCanvas Kiihdytetty Grafiikanlataus 
+ ​settings.client.resourcePacks Resurssipakkaukset 
+ ​settings.dev.header Kehittäjäasetukset 
+ ​settings.chat.header Chatti 
+ ​settings.chat.chat Näytä Chatti 
+ ​settings.chat.chat.focus Kun Kohdistettu 
+ ​settings.chat.unboxings Näytä skinien saamisviestit 
+ ​settings.chat.chatOp Chatin läpinäkyvyys 
+ ​settings.chat.chatBGOp Chatin taustan läpinäkyvyys 
+ ​settings.chat.chatHeight Chatin korkeus 
+ ​settings.chat.chatTimer Chatin Ajastin 
+ ​settings.chat.chatTextOutline Chatin ääriviivat 
+ ​settings.quality.updRate Maksimikuvataajuus 
+ ​settings.quality.rendDst Renderöintietöisyys 
+ ​settings.quality.trails Luotien Vanat 
+ ​settings.quality.yourTrails Luotiesi Vanat 
+ ​settings.quality.mflash Piipun Välähdys 
+ ​settings.quality.flap Tarkkuuskiväärin Läppä 
+ ​settings.quality.weaponShine Aseen Kiiltäminen 
+ ​settings.interface.hitInd Näytä Osumien Ilmoitukset 
+ ​settings.interface.hitColor Osuman Ilmoituksen Väri 
+ ​settings.interface.critCol Kriittisen Vahingon Väri 
+ ​settings.interface.header Käyttöliittymä 
+ ​settings.interface.scale Käyttöliittymän Koko 
+ ​settings.interface.theme Pelin Teema 
+ ​settings.interface.ui Näytä Käyttöliittymä 
+ ​settings.interface.rarityanim Kytke Harvinaisuuksien Animaatiot Pois 
+ ​settings.interface.spectators Näytä Katselijat 
+ ​settings.interface.speedOffX Nopeuslaskurin Sijainti X 
+ ​settings.interface.speedOffY Nopeuslaskurin Sijainti Y 
+ ​settings.interface.nametagScale Nimimerkkien Koko 
+ ​settings.interface.nametagStyle.health Ainoastaan HP-pisteet 
+ ​settings.interface.healthNum HP-pisteet Nimimerkissä 
+ ​settings.interface.xpBarCol2 XP-palkin Väri 2 
+ ​settings.medals.header Mitalit 
+ ​settings.medals.show Näytä Mitalit 
+ ​settings.medals.classic Vanhat Mitalit 
+ ​settings.medals.sound Soita Mitalin Ääni 
+ ​settings.medals.scale Mitalin Koko 
+ ​settings.medals.xOff Mitalin Sijainti X 
+ ​settings.medals.yOff Mitali Sijainti Y 
+ ​settings.crosshair.header Crosshair 
+ ​settings.crosshair.type.1 Dynaaminen 
+ ​settings.crosshair.type.2 Muodot 
+ ​settings.crosshair.type.5 Eritelty Risti 
+ ​settings.crosshair.image.width Kuvan Leveys 
+ ​settings.crosshair.image.height Kuvan Korkeus 
+ ​settings.crosshairm.header Crosshair (Puukkoa Heitettäessä)  
+ ​settings.crosshairm.type Tyyppi 
+ ​settings.crosshairm.type.1 Muodot 
+ ​settings.crosshairm.type.2 Kuva 
+ ​settings.crosshairm.type.3 Eritelty Risti
+ ​settings.crosshairm.style Tyyli 
+ ​settings.crosshairm.style.0 Risti 
+ ​settings.crosshairm.style.1 Ontto Ympyrä 
+ ​settings.crosshairm.style.2 Kiinteä Ympyrä 
+ ​settings.crosshairm.style.3 Ontto Neliö 
+ ​settings.crosshairm.style.4 Kiinteä Neliö  
+ ​settings.crosshairm.image Kuva  
+ ​settings.crosshairm.image.paste Crosshairin Kuva  
+ ​settings.crosshairm.image.width Kuvan Leveys  
+ ​settings.crosshairm.image.height Kuvan Korkeus  
+ ​settings.crosshairm.always Näytä Aina  
+ ​settings.crosshairm.color Väri  
+ ​settings.crosshairm.shadow.color Varjon Väri  
+ ​settings.crosshairm.shadow.thickness Varjon Paksuus 
+ ​settings.crosshairm.size Koko 
+ ​settings.crosshairm.thickness Paksuus 
+ ​settings.crosshairm.gap Etäisyys 
+ ​settings.crosshairm.dot Piste 
+ ​settings.crosshairm.opacity.use Käytä Mukautettua Läpinäkyvyyttä 
+ ​settings.crosshairm.opacity.custom Mukautettu Läpinäkyvyys 
+ ​settings.hitmarker.spacing Väli 
+ ​settings.hitmarker.hitmFad Häivytyksen Nopeus 
+ ​settings.hitmarker.custom.paste Osuman Kuvan URL 
+ ​settings.gameprivacy.header Pelaamisen Yksityisyys 
+ ​settings.gameprivacy.streamer Striimaajatila 
+ ​settings.gameprivacy.anon Anonyymi Tila 
+ ​settings.gameprivacy.hideGameTitle Piilota Pelin Otsikko 
+ ​settings.gameprivacy.showRegion Näytä Alueen Tiedot 
+ ​settings.gameprivacy.verified Näytä Verified-merkki 
+ ​settings.gameprivacy.premium Näytä Premium-merkki 
+ ​settings.gameprivacy.partner Näytä Yhteistyö-merkki 
+ ​settings.gameprivacy.rank Näytä Kilpailullisen Tasomerkki 
+ ​settings.gameprivacy.custom Näytä Kustomoitu Merkki 
+ ​settings.gameplay.header Pelaaminen 
+ ​settings.gameplay.gpSetts Peliasetukset 
+ ​settings.gameplay.sensitivityX Herkkyys X 
+ ​settings.gameplay.sensitivityY Herkkyys Y 
+ ​settings.gameplay.aimsensX Tähtäyksen Herkkyys X 
+ ​settings.gameplay.aimsensY Tähtäyksen Herkkyys Y 
+ ​settings.gameplay.challenge Haastava Moodi 
+ ​settings.gameplay.invert Peilaa Y-Akseli 
+ ​settings.controller.sensitivityX Herkkyys X 
+ ​settings.controller.disableCons Poista Ohjaimet Käytöstä 
+ ​settings.controller.sensitivityY Herkkyys Y 
+ ​settings.controller.aimsensX Tähtäyksen Herkkyys X 
+ ​settings.controller.aimsensY Tähtäyksen Herkkyys Y 
+ ​settings.controller.invert Peilaa Y-Akseli 
+ ​settings.mobile.header Puhelin 
+ ​settings.mobile.sensitivityX Herkkyys Y 
+ ​settings.mobile.sensitivityY Herkkyys Y 
+ ​settings.mobile.aimsensX Tähtäyksen Herkkyys X 
+ ​settings.mobile.aimsensY Tähtäyksen Herkkyys Y 
+ ​settings.mobile.invert Peilaa Y-Akseli 
+ ​settings.audio.header Äänet 
+ ​settings.audio.input Äänen/Mikrofonin Lähde 
+ ​settings.audio.mic Mikrofonin Ääni 
+ ​settings.audio.micquality Mikrofonin Laatu 
+ ​settings.viewmodel.header Näkymä 
+ ​settings.viewmodel.vmSetts Näkymän Asetukset 
+ ​settings.viewmodel.toggleAim Vaihda Tähtäys 
+ ​settings.viewmodel.xOff Aseen Sijainti X 
+ ​settings.viewmodel.yOff Aseen Sijainti Y 
+ ​settings.viewmodel.zOff Aseen Sijainti Z 
+ ​settings.viewmodel.weapSwapY Aseen Vaihto Y 
+ ​settings.viewmodel.weapReloadY Aseen Lataus Y 
+ ​settings.viewmodel.yOffADS Aseen Sijainti Y-Akselilla Tähdätessä 
+ ​settings.viewmodel.adsFovMlt FOVin Voimakkuus Tähdätessä 
+ ​settings.viewmodel.toggleADS Vaihda Aseen Tähtäysasento 
+ ​settings.editing.progOffX Edistymispalkin sijainti X 
+ ​settings.editing.progOffY Edistymispalkin Sijainti Y 
+ ​settings.editing.progm.color Latauspalkin Väri 
+ ​settings.editing.progmo.color Ylilatautumisen Väri 
+ ​settings.editing.progm.opacity Latauspalkin Läpinäkyvyys 
+ ​settings.editing.progm.scale Latauspalkin Koko 
+ ​settings.editing.progm.shadow Latauspalkin Varjo 
+ ​settings.editing.progmOffX Latauspalkin Sijainti X 
+ ​settings.editing.progmOffY Latauspalkin Sijainti Y 
+ ​settings.editing.progmRot Latauspalkin Rotaatio 
+ ​settings.editing.feedLimit Tappolistan Raja 
+ ​settings.editing.depth Syvyyskartta 
+ ​settings.editing.tracer.offset Tracerin Sijainti 
+ ​settings.mods.load Lataa Modeja 
+ ​settings.mods.auto Lataa Modit Automaattisesti 
+ settings.mods.dot Reticle-Kuva 
+ ​settings.mods.streak Tappoputkikuvakkeen Kuva 
+ ​settings.mods.streak.paste Tappoputkikuvakkeen Kuvan URL 
+ ​settings.social.bg Taustakuva 
+ ​settings.social.bg.paste Taustakuvan URL 
+ ​windows.servers.online {0} Pelissä
+ ​windows.servers.social {0} Hubissa 
+ ​windows.servers.editor {0} Editorissa 
+ ​windows.loadout.color.chat Nimen Väri Chatissa 
+ ​windows.loadout.color.hair Hiusten Väri 
+ ​windows.loadout.dye Värjäys 
+ ​windows.loadout.wrist Ranne 
+ ​windows.mods.load.header Lataa Modeja 
+ ​windows.mods.viewer Tarkastelija 
+ ​windows.controls.left Vasemmalle 
+ ​windows.controls.right Oikealle 
+ ​windows.controls.reload Aseen Lataaminen 
+ ​windows.controls.aim1 Tähtääminen 
+ ​windows.controls.header Vaihda Näppäimiä 
+ ​windows.controls.inspect Tutkiminen
+ ​windows.controls.sprayWheel Spray-Pyörä (Pidä Painettuna) 
+ ​windows.controls.jump Hyppääminen
+ ​windows.controls.chat Chatti 
+ ​windows.controls.kickVoteY Vote Kick [Kyllä] 
+ ​windows.controls.kickVoteN Vote Kick [Ei] 
+ ​windows.controls.kpdVoteY KPD Vote [Kyllä] 
+ ​windows.controls.kpdVoteN KPD Vote [Ei] 
+ ​windows.controls.specFree Vaihda Vapaaseen Tilaan 
+ ​windows.controls.specObj Vaihda Objektitilaan 
+ ​windows.controls.specFirst Vaihda Ensimmäiseen Persoonaan 
+ ​windows.controls.specNames Vaihda Pakotetut Nimet 
+ ​windows.controls.specFocus Vaihda Pelaajaan Keskittymiseen 
+ ​windows.controls.hidePlayers Piilota Pelaajat 
+ ​windows.controls.taunt0 Päästä Ääni (Psst) 
+ ​windows.controls.taunt1 Päästä Ääni (Whistle) 
+ ​windows.controls.taunt2 Päästä Ääni (Fart) 
+ ​windows.controls.taunt3 Päästä Ääni (Meme) 
+ ​windows.controls.taunt4 Päästä Ääni (WTF is This?) 
+ ​windows.controls.taunt5 Päästä Ääni (Right Now!) 
+ ​windows.controls.prop Valitse Prop (Hold) 
+ ​windows.controls.propRand Valitse Satunnainen Prop 
+ windows.host.header Luo Kustomoitu Peli 
+ windows.host.modes Valitse Pelimuodot 
+ ​windows.clans.login luodaksesi tai liittyäksesi Klaaniin! 
+ ​windows.store.market2 Krunker-kauppa 
+ ​windows.store.unboxed {0}{1} unboxasi 
+ ​windows.store.found {0}{1} löysi  
+ ​windows.reticle.req Taso {0} Vaadittu
+ ​windows.secondary.customOnly Vain Custom-Peleissä 
+ ​windows.secondary.premiumOnly Vain Premiumin Kanssa 
+ ​windows.secondary.req Taso {0} Vaadittu 
+ ​windows.join.code Syötä Pelin URL/Koodi 
+ ​windows.client.windows Windows-Asentaja 
+ ​windows.client.mac macOS-Asentaja 
+ ​windows.client.linux Linux-Asentaja 
+ ​matchmaker.GameFull Peli on täysi 
+ ​matchmaker.NoServersMatchQuery PELI PÄIVITTYY 
+ ​matchmaker.InvalidGameId Peliä ei löydetty. 
+ ​matchmaker.NoAvailableServers Serverit ovat maksimikapasiteetilla 
+ ​matchmaker.GameIssue Tuntematon Liittymisvika 
+ custom.map.upload.limit Epäonnistui. 500 000 Merkin Pelinjulkaisuraja 
+ ​server.config.minPlayers Minimi Pelaajamäärä
+ ​server.config.airStrf Suunnanvaihto Ilmassa 
+ ​server.config.strafeSpd Suunnanvaihdon Nopeus 
+ ​server.config.reSpd Aseen Lataamisen Nopeus 
+ server.config.allowSpect Katseleminen 
+ server.config.bDrop Luotien Putoaminen 
+ server.config.requireCaptcha Vaadi Captchaa 
+ server.config.kCams Tappokamerat 
+ server.config.aAnon Salli Anonyymiys 
+ server.config.noReload Ei Aseen Lataamista 
+ ​server.config.intermTmr Väliaika (s) 
+ ​server.config.hitBoxPad Osumalaatikon Pehmennys 
+ ​server.config.hpRegen Regenerointi 
+ ​server.config.limitClasses Rajoita Hahmoja 
+ ​server.config.chrgWeps Ladattavat Aseet 
+ ​social.user.maps Käyttäjän Pelit 
+ ​social.user.mods Käyttäjän Modit 
+ ​social.market.price.list Myyntihinta 
+ ​social.market.listed laitettu myyntiin {0} sitten 
+ ​social.market.list Laita Esine Myyntiin 
+ ​social.market.unlist Poista Esine Myynnistä 
+ ​social.market.unlisting Poistetaan Esinettä Myynnistä...
+ ​social.market.price.list Myyntihinta 
+ ​social.market.list Laita Esine Myyntiin 
+ ​social.market.unlist Poista Esine Myynnistä 
+ ​social.market.unlisting Poistetaan Esinettä Myynnistä... 
+ ​social.market.listed laitettu myyntiin {0} sitten 
+ ​social.market.wrist Ranne-esine
+ ​social.market.collectible Keräilyesine 
+ ​social.trading.submitting Lähetetään Tarjousta... 
+ ​stats.crouches Kyykyt 
+ ​stats.kpd Hyödylliset Ilmiannot
+ ​windows.host.default.settings Oletusasetusten käyttöön ottaminen mahdollistaa pelaajan edistymisen ja huipputulosten tallentamisen 
+ ​windows.store.wheels.sub Kokeile onneasi, pyöräytä ja saa mahdollisuus voitaa uusia skinejä! 
+ ​item.view.open Avaa Hahmon Tarkastelija 
+ ​item.sell.cant.krunker Esinettä ei voi myydä krunkerissa. 
+ ​item.sell.destroy Haluatko varmasti tuhota {0}? 
+ ​social.profile.assets Assetit [{0}] 
+ ​stats.airdrops.stolen Airdroppeja Varastettu 
+ ​settings.quality.flapAnim Tarkkuuskiväärin Läpän animaatio 
+ ​chall.name.hs Saa {0} headshottia {1} 
+ ​chall.name.ns Saa {0} no-scopea {1} 
+ ​chall.name.ls Saa {0} legshottia {1} 
+ ​chall.name.tmk Saa {0} puukonheittotappoa {1} 
+ ​chall.name.fb Saa {0} ensimmäistä tappoa {1} 
+ ​chall.name.mk Saa {0} melee-tappoa {1} 
+ ​chall.name.fk Saa {0} nyrkkitappoa {1} 
+ ​chall.name.qs Saa {0} quickscopea {1} 
+ ​chall.name.wb Saa {0} wallbangia {1} 
+ ​chall.name.airk Saa {0} mid-air-tappoa {1} 
+ ​chall.name.lns Saa {0} longshottia {1} 
+ ​chall.name.tk Saa {0} turbo-tappoa {1} 
+ ​chall.name.drk Saa {0} drifti-tappoa {1} 
+ ​chall.name.kstk Saa {0}:n tappoputki {1} 
+ ​chall.name.w Voita {0} ottelua 
+ ​chall.name.d Kuole {0} kertaa 
+ ​chall.name.score Saa {0} pistettä 
+ ​chall.name.dmg Tee {0} vahinkoa {1} 
+ ​chall.name.bzk Saa {0} buzz-tappoa {1} 
+ ​chall.name.cc Saa {0} läheltä piti-tappoa {1} 
+ ​chall.name.rk Saa {0} kostotappoa {1} 
+ ​`​;
