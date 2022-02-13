23b954fcc18c1e7413ecf5c3ea0a6ab6f277d31e
Matt Taylor <matt@idle.host>
2022-02-14 10:11:28 +1100

    Rebuild tr.js with en.js updates (v5.4.4)

--- tr.js
+++ tr.js
@@ -84,6 +84,8 @@ settings.none Ayarlar bulunamadı
 settings.backup Yedek Ayarlar
 settings.controls.change Tuş Ayarları
 settings.controls.press Herhangi bir tuşa basın
+settings.controls.leaderboard InGame Leaderboard Trigger Type
+settings.controls.voicechat Voicechat Trigger Type
 settings.local.header Localization <span id="requiresRestart"><span style="color: #eb5656">*</span> yeniden başlatma gerektirir</span>
 settings.local.region Varsayılan Bölge
 settings.local.lang Dil
@@ -92,6 +94,7 @@ settings.network.netRerouting Otomatik Yönlendirme (Ping'i Etkiler)
 settings.network.netRate Ağ Oranı (Hz)
 settings.network.lagComp Lag Gecikmesi
 settings.network.optNet Optimize Edilmiş Ağ
+settings.network.quickplay Force Quickplay
 settings.popup.header Popuplar
 settings.experimental.header Deneysel
 settings.experimental.holiday Noel Efektleri
@@ -646,6 +649,12 @@ windows.secondary.unlocked Açıldı
 windows.secondary.customOnly Sadece Özel
 windows.secondary.premiumOnly Sadece Premium
 windows.secondary.req lvl {0} gerektirir
+windows.skins.unlocked Unlocked
+windows.skins.customOnly Customs Only
+windows.skins.premiumOnly Premium Only
+windows.skins.classOnly Req {0}
+windows.skins.req Req lvl {0}
+windows.skins.default Default Skin
 windows.advertise.header Reklam yap
 windows.support.header Destek
 windows.players.header Oyuncu Listesi
@@ -911,4 +920,63 @@ chall.name.dmg {1}'da {0} hasar ver
 chall.name.bzk {1}'da {0} tane hızlı öldürme al 
 chall.name.cc {1}'da {0} tane dar kurtulma al 
 chall.name.rk {1}'da {0} tane intikam öldürmesi al
+event.doubleXP 2x Class XP enabled for this match
+ranked.join Joined Ranked Match. Abandoning a match will result in a temporary ban
+ranked.progress Game in Progress
+ranked.team Please join a Team
+ranked.switch Can't switch while in-game
+ranked.spectate Spectating Disabled in Ranked
+ranked.login Login to Spawn
+ranked.sync Missing Sync Data
+ranked.team.spawn Please join a Team to Spawn
+ranked.class.spawn Please select a Class to Spawn
+modes.round.win Your Team has secured this round
+modes.round.loss This round is no longer winnable
+modes.round.switch Switching Sides. Prepare for the next Round
+modes.trade.success Trade Successful
+modes.prophunt.limit [Prop Hunt] You are out of prop switches
+modes.prophunt.left [Prop Hunt] You have {0} switches remaining
+modes.prophunt.rotation.on [Prop Hunt] Rotation Enabled
+modes.prophunt.rotation.off [Prop Hunt] Rotation Disabled
+modes.prophunt.rotation.reset [Prop Hunt] Rotation reset.
+commands.login Login to use this command
+commands.connected {0} connected, {1} in menu
+commands.host {0} is the current Host.
+commands.noclip.on Noclip - Enabled
+commands.noclip.off Noclip - Disabled
+commands.god.on God mode - Enabled
+commands.god.off God mode - Disabled
+commands.god.on.other God mode - Enabled for {0}
+commands.god.off.other God mode - Disabled for {0}
+commands.smite.on Smite - Enabled
+commands.smite.off Smite - Disabled
+commands.smite.on.other Smite - Enabled for {0}
+commands.smite.off.other Smite - Disabled for {0}
+commands.perk Gave {0} {1}
+commands.maxperk Gave {0} MAX {1}
+commands.unlimited.on Unlimited Ammo - Enabled
+commands.unlimited.off Unlimited Ammo - Disabled
+commands.unlimited.on.other Unlimited Ammo - Enabled for {0}
+commands.unlimited.off.other Unlimited Ammo - Disabled for {0}
+commands.spraybypass.on Unlimited Ammo - Enabled
+commands.spraybypass.off Unlimited Ammo - Disabled
+commands.spraybypass.on.other Unlimited Ammo - Enabled for {0}
+commands.spraybypass.off.other Unlimited Ammo - Disabled for {0}
+commands.suspect.invalid Invalid suspect info
+commands.suspect.menu Suspect in menu
+commands.suspect.left Suspect has left
+commands.suspect.found Suspect Found. Spectating: {0}, Spawned: {1}, Name: {2}
+commands.forfeit.next Must play atleast 1 round
+commands.forfeit.wait Wait till match starts to forfeit
+commands.forfeit.later Please try again in a few seconds
+commands.forfeit.voted You already voted.
+commands.forfeit.limit You can only start one vote per game
+commands.forfeit.ended Forfeiting Game
+commands.forfeit.ended.other Other Team Forfeited
+server.message.join {0} joined the game
+server.message.left {0} left the game
+server.message.kicked {0} was kicked
+server.message.banned {0} was banned
+server.message.votekicked {0} was vote kicked
+server.message.arrested {0} was arrested by the K.P.D
 `;
